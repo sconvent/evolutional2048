981a14b7141557d3bcda1cd7fa819798618718b0
Simon Convent <s.convent@gmail.com>
2017-08-20 01:21:04 +0200

    Add save/load capabilities to Neural Network

--- src/de/convent/evolutional2048/neuralNetwork/NeuralNetwork.java
+++ src/de/convent/evolutional2048/neuralNetwork/NeuralNetwork.java
@@ -1,7 +1,14 @@
 package de.convent.evolutional2048.neuralNetwork;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import Jama.Matrix;
 import de.convent.evolutional2048.neuralNetwork.activationFunctions.ActivationFunction;
+import de.convent.evolutional2048.util.Direction;
 
 public class NeuralNetwork
 {
@@ -17,13 +24,72 @@ public class NeuralNetwork
 		}
 	}
 
-	public Matrix calculate(Matrix input)
+	public NeuralNetwork(String path)
+	{
+		load(path);
+	}
+
+	public Direction calculate(Matrix input)
 	{
 		Matrix current = input;
 		for(int i = 0; i < weights.length; i++)
 		{
 			current = weights[i].times(current);
 		}
-		return current;
+		int maxIndex = 0;
+		if(current.getArray()[0][1] > current.getArray()[0][maxIndex])
+			maxIndex = 1;
+		if(current.getArray()[0][2] > current.getArray()[0][maxIndex])
+			maxIndex = 2;
+		if(current.getArray()[0][3] > current.getArray()[0][maxIndex])
+			maxIndex = 3;
+
+		if(maxIndex == 0)
+			return Direction.UP;
+		else if(maxIndex == 0)
+			return Direction.RIGHT;
+		else if(maxIndex == 0)
+			return Direction.DOWN;
+		else
+			return Direction.LEFT;
+	}
+
+	public void save(String path)
+	{
+		try
+		{
+			FileOutputStream fileOut = new FileOutputStream(path);
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(weights);
+			out.close();
+			fileOut.close();
+			System.out.printf("Serialized data is saved in " + path);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void load(String path)
+	{
+		try
+		{
+			FileInputStream fileIn = new FileInputStream(path);
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			weights = (Matrix[]) in.readObject();
+			in.close();
+			fileIn.close();
+		}
+		catch (IOException i)
+		{
+			i.printStackTrace();
+			return;
+		}
+		catch (ClassNotFoundException c)
+		{
+			c.printStackTrace();
+			return;
+		}
 	}
 }
